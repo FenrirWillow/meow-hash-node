c5c9e41fae3a66aa4ab493ead2e6c8b21390ed33
Stefan Bobev <stefanbobev@hotmail.com>
2019-11-22 06:50:08 +0000

    Normalise the style of the C++ file.

--- lib/cpp/main.cpp
+++ lib/cpp/main.cpp
@@ -9,20 +9,20 @@ Napi::String CreateHash(const Napi::CallbackInfo& info) {
 		Napi::TypeError::New(env, "Buffer expected").ThrowAsJavaScriptException();
 	}
 
-	Napi::Buffer<uint8_t> Buffer = info[0].As<Napi::Buffer<uint8_t>>();
+	Napi::Buffer<uint8_t> buffer = info[0].As<Napi::Buffer<uint8_t>>();
 	meow_u128 Hash = MeowHash(MeowDefaultSeed, buffer.Length(), buffer.Data());
 
-	char HashStringBuffer[36];
+	char hashStringBuffer[36];
 	snprintf(
-		HashStringBuffer,
-		sizeof(HashStringBuffer),
+		hashStringBuffer,
+		sizeof(hashStringBuffer),
 		"%08X-%08X-%08X-%08X",
 		MeowU32From(Hash, 3),
 		MeowU32From(Hash, 2),
 		MeowU32From(Hash, 1),
 		MeowU32From(Hash, 0)
 	);
-	return Napi::String::New(env, HashStringBuffer, 36);
+	return Napi::String::New(env, hashStringBuffer, 36);
 }
 
 Napi::Boolean CompareBuffers(const Napi::CallbackInfo& info) {
@@ -36,9 +36,9 @@ Napi::Boolean CompareBuffers(const Napi::CallbackInfo& info) {
 	Napi::Buffer<uint8_t> bufferA = info[0].As<Napi::Buffer<uint8_t>>();
 	Napi::Buffer<uint8_t> bufferB = info[0].As<Napi::Buffer<uint8_t>>();
 
-	meow_u128 HashA = MeowHash(MeowDefaultSeed, bufferA.Length(), bufferA.Data());
-	meow_u128 HashB = MeowHash(MeowDefaultSeed, bufferB.Length(), bufferB.Data());
-	return Napi::Boolean::New(env, MeowHashesAreEqual(HashA, HashB));
+	meow_u128 hashA = MeowHash(MeowDefaultSeed, bufferA.Length(), bufferA.Data());
+	meow_u128 hashB = MeowHash(MeowDefaultSeed, bufferB.Length(), bufferB.Data());
+	return Napi::Boolean::New(env, MeowHashesAreEqual(hashA, hashB));
 }
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
