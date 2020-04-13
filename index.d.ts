0c81b745a3d1d64210ec57f349923226636041f1
Stefan Bobev <stefanbobev@hotmail.com>
2020-04-13 22:07:36 +0100

    Extend the typescript definitions.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,19 @@
+declare class MeowHashNativeStream {
+  /**
+   * Appends the given buffer to the hash stream, extending the hash
+   * using the value of the bytes in the buffer.
+   * @param b {Buffer} The buffer to extend the hash with.
+   * @returns void
+   */
+  public absorb(b: Buffer): void;
+
+  /**
+   * Closes the underlying hash stream, returning the hash as a string.
+   * @returns string
+   */
+  public end(): string;
+}
+
 export function CreateHash(b: Buffer): string
 
 export function CompareBuffers(a: Buffer, b: Buffer): boolean
